6e8f63dec376ecf4194d5ee5d4eb29eaad3dc737
t.t <tomoyo.ggg@gmail.com>
2021-04-27 13:21:16 +0000

    timeline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,13 @@
 
 @section('content')
     @if (Auth::check())
+    
         <div class="row">
             <aside class="col-sm-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
-                    </div>
-                    <div class="card-body">
-                        {{-- 認証済みユーザのメールアドレスをもとにGravatarを取得して表示 --}}
-                        <img class="rounded img-fluid" src="{{ Gravatar::get(Auth::user()->email, ['size' => 500]) }}" alt="">
-                    </div>
-                </div>
+                {{-- ユーザ情報 --}}
+                @include('users.card')
             </aside>
+            
             <div class="col-sm-8">
                 {{-- 投稿フォーム --}}
                 @include('microposts.form')
@@ -21,7 +16,9 @@
                 @include('microposts.microposts')
             </div>
         </div>
+        
     @else
+    
         <div class="center jumbotron">
             <div class="text-center">
                 <h1>Welcome to the Microposts</h1>
@@ -29,5 +26,6 @@
                 {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
+        
     @endif
 @endsection
